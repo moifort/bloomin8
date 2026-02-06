0f8ea906ed2419e4daa492413ce2fbfdd54d49c9
Thibaut Mottet <thibaut.mottet@gmail.com>
2026-02-07 00:14:03 +0100

    chore: improve logger

--- server/plugins/logger.ts
+++ server/plugins/logger.ts
@@ -1,5 +1,14 @@
 export default defineNitroPlugin((nitroApp) => {
   nitroApp.hooks.hook('request', (event) => {
-    console.log('on request', event.path)
+    console.log(`${new Date().toISOString()} on request `, event.path)
+  })
+  nitroApp.hooks.hook('beforeResponse', (event, { body }) => {
+    console.log(`${new Date().toISOString()} on response`, event.path, { body })
+  })
+
+  nitroApp.hooks.hook('afterResponse', (event, response) => {
+    console.log(`${new Date().toISOString()} on after response`, event.path, {
+      body: response?.body,
+    })
   })
 })
