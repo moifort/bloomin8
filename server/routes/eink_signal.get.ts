dec2aaadabdc576b2d5abf3ccda88a66feef348f
Thibaut Mottet <thibaut.mottet@gmail.com>
2026-02-10 16:03:44 +0100

    feat: add playlist

--- server/routes/eink_signal.get.ts
+++ server/routes/eink_signal.get.ts
@@ -1,4 +1,9 @@
+import { Canvas } from '~/canvas/index'
+import { config } from '~/config/index'
+
 export default eventHandler(async () => {
+  const { canvasUrl, serverUrl } = config()
+  await Canvas.sleep(canvasUrl, serverUrl)
   return {
     status: 200,
     message: 'Feedback recorded',
