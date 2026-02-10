05200f95e8f70afea1a0aa89a76e7d8f51b73262
Thibaut Mottet <thibaut.mottet@gmail.com>
2026-02-10 16:09:05 +0100

    chore: ad missing env

--- server/routes/eink_signal.get.ts
+++ server/routes/eink_signal.get.ts
@@ -1,9 +1,4 @@
-import { Canvas } from '~/canvas/index'
-import { config } from '~/config/index'
-
 export default eventHandler(async () => {
-  const { canvasUrl, serverUrl } = config()
-  await Canvas.sleep(canvasUrl, serverUrl)
   return {
     status: 200,
     message: 'Feedback recorded',
