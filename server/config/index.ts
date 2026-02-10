88f60e6aa07f1800808ba3679846615303f6c1ca
Thibaut Mottet <thibaut.mottet@gmail.com>
2026-02-10 16:47:36 +0100

    chore: display configuration on startup

--- server/config/index.ts
+++ server/config/index.ts
@@ -2,7 +2,6 @@ import { CanvasUrl, Hour, ServerUrl } from '~/config/primitives'
 
 export const config = () => {
   const config = useRuntimeConfig()
-  console.log(`${JSON.stringify(config)}`)
   return {
     canvasUrl: CanvasUrl(config.canvasUrl),
     serverUrl: ServerUrl(config.serverUrl),
